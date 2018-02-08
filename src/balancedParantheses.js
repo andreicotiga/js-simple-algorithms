3cb4dd95a4b6a5457457d62c0ebfec8603660ad4
Andrei Cotiga <andrei.cotiga@yahoo.com>
2018-02-08 17:14:02 +0200

    fix tests

--- src/balancedParantheses.js
+++ src/balancedParantheses.js
@@ -7,11 +7,7 @@ const balancedParantheses = (input) => {
 		['{', '}'],
 		['[', ']']
 	]);
-<<<<<<< master
-	let foundOpen = [];
-=======
 	let openParanthesesFound = [];
->>>>>>> renamed symbol to be more readable
 
 	let open = Array.from(parantheses.keys());
 	let close = Array.from(parantheses.values());
@@ -19,21 +15,12 @@ const balancedParantheses = (input) => {
 	for (let c of input) {
 
 		if (open.indexOf(c) >= 0) {
-<<<<<<< master
-			foundOpen.push(c);
-		} else {
-			if (close.indexOf(c) >= 0) {
-
-				if (foundOpen.length > 0 && parantheses.get(foundOpen[foundOpen.length - 1]) === c) {
-					foundOpen.pop();
-=======
 			openParanthesesFound.push(c);
 		} else {
 			if (close.indexOf(c) >= 0) {
 
 				if (openParanthesesFound.length > 0 && parantheses.get(openParanthesesFound[openParanthesesFound.length - 1]) === c) {
 					openParanthesesFound.pop();
->>>>>>> renamed symbol to be more readable
 				} else {
 					return false;
 				}
@@ -41,11 +28,7 @@ const balancedParantheses = (input) => {
 		}
 	}
 
-<<<<<<< master
-	return foundOpen.length == 0;
-=======
 	return openParanthesesFound.length == 0;
->>>>>>> renamed symbol to be more readable
 }
 
 export default balancedParantheses;
